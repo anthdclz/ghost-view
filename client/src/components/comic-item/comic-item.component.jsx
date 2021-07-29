ed8d17092e3c6285f5426f888b3708753a890261
Anthony dela Calzada <anthony.delacalzada@gmail.com>
2021-07-29 10:24:27 -0700

    Add react-lazyload to ComicItem

--- client/src/components/comic-item/comic-item.component.jsx
+++ client/src/components/comic-item/comic-item.component.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import LazyLoad from 'react-lazyload';
+
 import './comic-item.styles.scss';
 
 const ComicItem = ({comic: {images, urls}}) => {
-    const imagePath = `${images[0].path}/${urls[0].type}.${images[0].extension}`;
-    return (
-        <div 
-            className='comic-item'
-            style={{
-                backgroundImage: `url(${imagePath})`
-            }} 
-        >
-        </div>        
-    );
+        const imagePath = `${images[0].path}/${urls[0].type}.${images[0].extension}`;
+        return (
+            <LazyLoad height={268} once offset={100}>
+            <div 
+                className='comic-item'
+                style={{
+                    backgroundImage: `url(${imagePath})`
+                }} 
+            >
+            </div>
+            </LazyLoad>        
+        );
 }
 export default ComicItem;
