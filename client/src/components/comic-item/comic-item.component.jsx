00b19a432dfe65c86328a7c215954f769e8f75ca
Anthony dela Calzada <anthony.delacalzada@gmail.com>
2021-07-29 10:54:40 -0700

    Comic image res decrs to uncanny

--- client/src/components/comic-item/comic-item.component.jsx
+++ client/src/components/comic-item/comic-item.component.jsx
@@ -3,8 +3,8 @@ import LazyLoad from 'react-lazyload';
 
 import './comic-item.styles.scss';
 
-const ComicItem = ({comic: {images, urls}}) => {
-        const imagePath = `${images[0].path}/${urls[0].type}.${images[0].extension}`;
+const ComicItem = ({comic: {thumbnail}}) => {
+        const imagePath = `${thumbnail.path}/portrait_uncanny.${thumbnail.extension}`;
         return (
             <LazyLoad height={268} once offset={100}>
             <div 
